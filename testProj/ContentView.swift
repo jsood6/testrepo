19ae1af327dd6b9cf5032b219be55fb8efb63076
Jigyasaa Sood <jigyasaasood@Jigyasaas-MacBook-Pro.local>
2020-02-04 02:06:26 -0800

    Initial Commit

---
+++ testProj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testProj
+//
+//  Created by Jigyasaa Sood on 2/4/20.
+//  Copyright © 2020 Jigyasaa Sood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
