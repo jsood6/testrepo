4e4a55e50314d1e84104f75cb3295fc80798a925
Jigyasaa Sood <jigyasaasood@Jigyasaas-MacBook-Pro.local>
2020-02-04 02:09:30 -0800

    made some changes

--- testProj/ContentView.swift
+++ testProj/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
         Text("Hello, World!")
     }
 }
-
+//made some changes...
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
